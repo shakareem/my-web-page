f1348812967165688a96f186176637d14da6c00b
Karim Shakirov <karimshak06@gmail.com>
2024-12-24 10:09:15 +0300

    add: auth
    Signed-off-by: Karim Shakirov <karimshak06@gmail.com>

---
+++ go.mod
@@ -0,0 +1,14 @@
+module main
+
+go 1.22.9
+
+require (
+	cloud.google.com/go/compute/metadata v0.3.0 // indirect
+	github.com/gorilla/context v1.1.2 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/pat v1.0.2 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.1.1 // indirect
+	github.com/markbates/goth v1.80.0 // indirect
+	golang.org/x/oauth2 v0.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,30 +2,101 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
-	"text/template"
+	"os"
+	"sort"
+
+	"github.com/gorilla/pat"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/github"
+	"github.com/markbates/goth/providers/google"
+	"github.com/markbates/goth/providers/vk"
 )
 
-func home_page(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("index.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	err = tmpl.Execute(w, nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
 func main() {
-	fs := http.FileServer(http.Dir("src"))
-	http.Handle("/src/", http.StripPrefix("/src/", fs))
+	var (
+		google_key   = "649218385986-q3fq9bu2574qfc29qoudmrd7sl3e58ms.apps.googleusercontent.com"
+		goole_secret = "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"
+	)
 
-	http.HandleFunc("/", home_page)
+	goth.UseProviders(
+		google.New(google_key, goole_secret, "http://localhost:3000/auth/google/callback"),
+		github.New(os.Getenv("GITHUB_KEY"), os.Getenv("GITHUB_SECRET"), "http://localhost:3000/auth/github/callback"),
+		vk.New(os.Getenv("VK_KEY"), os.Getenv("VK_SECRET"), "http://localhost:3000/auth/vk/callback"),
+	)
 
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("Error starting server:", err)
+	m := map[string]string{
+		"github": "Github",
+		"google": "Google",
+		"vk":     "VK",
 	}
+	var keys []string
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	providerIndex := &ProviderIndex{Providers: keys, ProvidersMap: m}
+
+	p := pat.New()
+
+	p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
+
+		user, err := gothic.CompleteUserAuth(res, req)
+		if err != nil {
+			fmt.Fprintln(res, err)
+			return
+		}
+		t, _ := template.New("").Parse(userTemplate)
+		t.Execute(res, user)
+	})
+
+	p.Get("/logout/{provider}", func(res http.ResponseWriter, req *http.Request) {
+		gothic.Logout(res, req)
+		res.Header().Set("Location", "/")
+		res.WriteHeader(http.StatusTemporaryRedirect)
+	})
+
+	p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
+		// try to get the user without re-authenticating
+		if gothUser, err := gothic.CompleteUserAuth(res, req); err == nil {
+			t, _ := template.New("foo").Parse(userTemplate)
+			t.Execute(res, gothUser)
+		} else {
+			gothic.BeginAuthHandler(res, req)
+		}
+	})
+
+	p.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		t, _ := template.ParseFiles("index.html")
+		t.Execute(res, providerIndex)
+	})
+
+	p.PathPrefix("/src/").Handler(http.StripPrefix("/src/", http.FileServer(http.Dir("src")))) // for css
+
+	// Start the server
+	log.Println("listening on localhost:3000")
+	log.Fatal(http.ListenAndServe(":3000", p))
 }
+
+type ProviderIndex struct {
+	Providers    []string
+	ProvidersMap map[string]string
+}
+
+var userTemplate = `
+<p><a href="/logout/{{.Provider}}">logout</a></p>
+<p>Name: {{.Name}} [{{.LastName}}, {{.FirstName}}]</p>
+<p>Email: {{.Email}}</p>
+<p>NickName: {{.NickName}}</p>
+<p>Location: {{.Location}}</p>
+<p>AvatarURL: {{.AvatarURL}} <img src="{{.AvatarURL}}"></p>
+<p>Description: {{.Description}}</p>
+<p>UserID: {{.UserID}}</p>
+<p>AccessToken: {{.AccessToken}}</p>
+<p>ExpiresAt: {{.ExpiresAt}}</p>
+<p>RefreshToken: {{.RefreshToken}}</p>
+`
